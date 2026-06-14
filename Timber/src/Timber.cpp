bc1e69224c0e001c87cb723d3b5c6f8d827ec31e
yberramd <younesberramdane1@gmail.com>
2026-06-14 13:40:06 +0200

    Adding bckground

--- Timber/src/Timber.cpp
+++ Timber/src/Timber.cpp
@@ -6,16 +6,35 @@
 
 int main()
 {
-    sf::Window window(sf::VideoMode({ 1920, 1080 }), "Timber", sf::Style::Default);
+    sf::RenderWindow window(sf::VideoMode({ 1920, 1080 }), "Timber", sf::Style::Default, sf::State::Fullscreen);
+
+    sf::Texture textureBackground("graphics\\background.jpg");
+
+    sf::Sprite spriteBackground(textureBackground);
+
+    //In Game Variables :
+
+    int playerScore = 0;
+
+    bool isAlive = true;
 
     while (window.isOpen()) {
 
         while (const std::optional event = window.pollEvent()) {
             if (event->is<sf::Event::Closed>())
                 window.close();
+            else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>()) {
+                if (keyPressed->scancode == sf::Keyboard::Scancode::Escape)
+                    window.close();
+            }
         }
+        
+        window.clear();
 
+        window.draw(spriteBackground);
+
+        window.display();
     }
-
+    
     return 0;
 }
